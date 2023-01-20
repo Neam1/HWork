cfd8569e4f8c13624e0a9eaf8f87f0650b5e702e
Neam1 <kirishka12@yandex.ru>
2023-01-20 10:43:11 +0300

    done HW

---
+++ work1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает 2 числа и выдаёт, какое число >, а какое <.
+//Заранее извиняюсь, за то что чуть более нагрузил программу. Но мне кажется так выглядит приятнее.
+Console.Clear();
+Console.WriteLine("Введите два целых числа чтобы сравнить эти числа: ");
+Console.Write("Первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+    Console.WriteLine($"Первое число больше и это число {a}");
+else if (a == b)
+    Console.WriteLine("Вы указали одинаковые числа"); 
+else 
+    Console.WriteLine($"Второе число больше и это число {b}");
+
---
+++ work2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.WriteLine("Введите три целых числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (a != max || b != max || c != max) // неуверен что так верно.
+    Console.WriteLine($"Максимальное число: {max}");
+else
+    Console.WriteLine("Все введённые числа одинаковы!"); 
+// не смог разобраться как вернуть пользователя обратно ко вводу
+// так же не смог корректно выводить сообщение о двух max числах
